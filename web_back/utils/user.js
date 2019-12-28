42f178d1a6291472ee806232ba83e4da94a44596
chen1972 <625100523@qq.com>
2019-12-28 15:02:07 +0800

    将操作的dom元素利用回调函数放在原来的页面

--- web_back/utils/user.js
+++ web_back/utils/user.js
@@ -17,21 +17,19 @@ var user = {
 
     },
     logout: function () {
-        $('.icon-tuichu').parent('a').on('click', function () {
-            $.post(baseUrl + '/admin/logout', function (res) {
-                alert(res.msg)
-                if (res.code === 200) {
-                    location.href = 'login.html'
-                }
-            })
+      
+        $.post(baseUrl + '/admin/logout', function (res) {
+            alert(res.msg)
+            if (res.code === 200) {
+                location.href = 'login.html'
+            }
         })
     },
-    userinformation: function () {
+    userinformation: function (callback) {
         $.get(baseUrl + '/admin/getuser', function (res) {
-            console.log(res);
-
-            $('.user_info').children('img').attr('src', res.data.user_pic).siblings('span').html('欢迎&nbsp;&nbsp;' + res.data.nickname)
-            $('.user_center_link').children('img').attr('src', res.data.user_pic)
+            // console.log(res);
+            callback(res)
+           
         })
     }
 }
